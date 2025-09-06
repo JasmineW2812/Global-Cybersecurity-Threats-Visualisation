67531353ede9457a61c243091819c2a36f586659
JasmineW2812 <Jasmine.Walker2@student.shu.ac.uk>
2025-09-06 17:08:21 +0100

    interactive and map data

--- app.R
+++ app.R
@@ -1,29 +1,63 @@
 library(shiny)
 library(readr)
 library(ggplot2)
+library(dplyr)
 
-Cybersecurity<- read_csv("Global_Cybersecurity_Threats_2015-2024.csv")
+data(World) 
+
+Cybersecurity <- read_csv("Global_Cybersecurity_Threats_2015-2024.csv") %>%
+  mutate(Country = case_when(
+    Country == "USA" ~ "United States of America",
+    Country == "UK" ~ "United Kingdom",
+    TRUE ~ Country
+  ))
+
 # UI
 ui <- fluidPage(
   
   titlePanel("Global Cybersecurity Threats (2015-2024)"),
   
-  mainPanel(
-    plotOutput("attackCountPlot"),
-  )
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("range","Year:",
+                  min = 2015, max= 2024, step=1, value=2014)
+    ),
   
+    mainPanel(
+      plotOutput("attackCountPlot"),
+      plotOutput("map")
+
+    )
+  )
 )
 
 # Server logic
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  filtered_data <- reactive({
+    Cybersecurity %>%
+      filter(Year == input$range)
+  })
+  
+  World_data <- reactive({
+    World %>%
+      left_join(filtered_data(), by = c("name" = "Country"))
+  })
   
   
   output$attackCountPlot <- renderPlot({
-    ggplot(Cybersecurity, aes(Year))+
-      geom_bar()
+    ggplot(filtered_data(), aes(x=`Attack Type`, fill =`Security Vulnerability Type`)) +
+      geom_bar()+
+      xlab("Attack Type") +
+      ylab("Number of Attacks") 
     
   })
   
+  output$map <- renderPlot({
+    qtm(World_data(), fill='Number of Affected Users')
+      
+    
+  })
   
 }
 
